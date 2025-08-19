3fe8e3960fb3b2fddd0cfb064e3f1866f0c661e8
zzmjeremy <zhuominzhucontact@gmail.com>
2025-08-19 13:08:54 -0700

    Organized the Structure and Added the Background

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import PatientProfile from './pages/PatientProfile.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/patients/:id',
+    element: <PatientProfile />,
+  },
+  {
+    path: '/',
+    element: <App />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
---
+++ src/pages/PatientProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PatientProfile.css';
+
+function PatientProfile() {
+  return (
+    <div className="patient-profile">
+      
+    </div>
+  );
+}
+
+export default PatientProfile;
